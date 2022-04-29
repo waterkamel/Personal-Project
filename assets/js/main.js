d5ee97521e8cb38b2e44693aa6e58f5763854c55
waterkamel <makaumanu01@gmail.com>
2022-04-29 12:24:30 +0300

    Js file

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+
+// show menu
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+// hide menu
+closeBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none'; 
+})
+
+
+// active nav item
+const navItems = menu.querySelectorAll('li');
+
+const changeActiveItem = () => {
+    navItems.forEach(item => {
+        const link = item.querySelector('a');
+        link.classList.remove('active');
+    })
+}
+
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.addEventListener('click', () => {
+        changeActiveItem();
+        link.classList.add('active');
+    })
+})
+
+// show/hide skills items
+const skillItems = document.querySelectorAll('section.skills .skill');
+
+skillItems.forEach(skill => {
+    skill.querySelector('.head').addEventListener('click', () => {
+        skill.querySelector('.items').classList.toggle('show-items');
+    })    
+})
+
+// add box shadow on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('show-box-shadow',window.scrollY > 0)
+})
